da11e2a8be4b88f255d53beb397f7b246f477205
Dmitry Zhechkov <dzhechkov@yandex-team.ru>
2023-11-27 12:12:05 +0300

    3 commit

--- YaGPT-RAG-bot.py
+++ YaGPT-RAG-bot.py
@@ -120,12 +120,12 @@ def main():
         ''')
 
     global  yagpt_folder_id, yagpt_api_id, yagpt_api_key, mdb_os_ca, mdb_os_pwd, mdb_os_hosts, mdb_os_index_name    
-    yagpt_folder_id = st.sidebar.text_input("YAGPT_FOLDER_ID")
-    yagpt_api_id = st.sidebar.text_input("YAGPT_API_ID")
+    yagpt_folder_id = st.sidebar.text_input("YAGPT_FOLDER_ID", type='password')
+    yagpt_api_id = st.sidebar.text_input("YAGPT_API_ID", type='password')
     yagpt_api_key = st.sidebar.text_input("YAGPT_API_KEY", type='password')
     mdb_os_ca = MDB_OS_CA
     mdb_os_pwd = st.sidebar.text_input("MDB_OpenSearch_PASSWORD", type='password')
-    mdb_os_hosts = st.sidebar.text_input("MDB_OpenSearch_HOSTS через 'запятую' ").split(",")
+    mdb_os_hosts = st.sidebar.text_input("MDB_OpenSearch_HOSTS через 'запятую' ", type='password').split(",")
     mdb_os_index_name = st.sidebar.text_input("MDB_OpenSearch_INDEX_NAME")
 
     # Параметры chunk_size и chunk_overlap
